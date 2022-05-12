9d028344521fc1b681659c43b18e346b1dbeaa6e
Luca Mülke <67710173+lucamuelke@users.noreply.github.com>
2022-05-12 14:39:56 +0200

    Updated python file for windows

--- workspace/cbs lab 2/date_serial.py
+++ workspace/cbs lab 2/date_serial.py
@@ -1,9 +1,14 @@
 import serial
+import sys
 from datetime import datetime as dt
 
-usb_device : str = "/dev/tty.usbmodem1103"
 
-monitor = serial.Serial(usb_device)
+if sys.platform == "win32":
+    usb_device_win : str = "COM8"
+    monitor = serial.Serial(usb_device_win)
+else:
+    usb_device_un : str = "/dev/tty.usbmodem1103"
+    monitor = serial.Serial(usb_device_un)
 
 current_time = dt.today()
 time_str = current_time.strftime("%Y-%m-%d_%H-%M-%Sx").encode("utf-8")
