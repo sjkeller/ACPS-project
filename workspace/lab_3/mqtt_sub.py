5d6993bcd9d9bc7ce06a26a4dd38e9fa22cdc458
Sergej <35032543+sjkeller@users.noreply.github.com>
2022-06-20 14:30:49 +0200

    added mqtt python file

---
+++ workspace/lab_3/mqtt_sub.py
@@ -0,0 +1,22 @@
+import paho.mqtt.client as mq
+import time
+
+def on_connect(client, userdata, flags, rc):
+	print("connected with result code", str(rc))
+	client.subscribe("acps/receive_from/lora_10")
+	
+def on_message(client, userdata, msg):
+	print(msg.topic, str(msg.payload))
+	
+client = mq.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect_async("134.28.36.100", 1883, 60)
+client.loop_start()
+
+while True:
+	user_input = input("enter a message pls ")
+	client.publish("acps/send_to/lora_10", user_input)
+	print("published message", user_input)
+
