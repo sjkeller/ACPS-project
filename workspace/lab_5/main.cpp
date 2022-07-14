07e42650242277e0830d5e85c4b70610324f4931
Sergej <35032543+sjkeller@users.noreply.github.com>
2022-07-14 22:55:13 +0200

    added energy measure

--- workspace/lab_5/main.cpp
+++ workspace/lab_5/main.cpp
@@ -1,4 +1,5 @@
 #include "mbed.h"
+#include <chrono>
 #include <cstdio>
 #include <cstring>
 #include <stdio.h>
@@ -8,6 +9,8 @@
 #include "lorawan/system/lorawan_data_structures.h"
 #include "events/EventQueue.h"
 #include "BMP280.h"
+#include "SDBlockDevice.h"
+#include "FATFileSystem.h"
 
 // Application helpers
 #include "lora_radio_helper.h"
@@ -25,6 +28,7 @@
 
 #define ACT_MODE_CUR 4.5
 #define SLP_MODE_CUR 1.1
+#define DSLP_MODE_CUR 0.0045
 #define LORA_RX_CUR 10.0
 #define LORA_TX_CUR 29.0
 
@@ -39,19 +43,34 @@ uint8_t tx_buffer[70];
 uint16_t packet_len;
 float temp = 0.0;
 float pres = 0.0;
-short pwr_status = ECO_MODE;
+short pwr_status = HARVEST_MODE;
+chrono::seconds dyn_samp_rate = LORA_TIMER;
 
 // main() runs in its own thread in the OS
 AnalogIn solar_voltage(A2);
 AnalogIn cap_voltage(A0);
 
+// sensor initialization
 I2C* bus = new I2C(SDA_PIN, SCL_PIN);
 BMP280* sensor = new BMP280(bus, SENSOR_ID);
 
+// sdcard initialization
+SDBlockDevice sd(MBED_CONF_SD_SPI_MOSI,
+                 MBED_CONF_SD_SPI_MISO,
+                 MBED_CONF_SD_SPI_CLK,
+                 MBED_CONF_SD_SPI_CS);
+FATFileSystem fs("sd", &sd);
+
 float solar_cur_val;
 float cap_vol_val;
-float avg_cap_vol_val;
+float avg_cap_curr_val = 0.0;
 float soc;
+float node_pwr = 0.0;
+float node_curr = 0.0;
+
+float active_time;
+float sleep_time;
+float deep_sleep_time;
 
 short mode = 0;
 
@@ -78,10 +97,32 @@ static lorawan_app_callbacks_t callbacks;
 static lorawan_connect_t connection;
 
 static void check_power() {
+    mbed_stats_cpu_t stats;
+    mbed_stats_cpu_get(&stats);
+    active_time = (float) (stats.uptime - stats.sleep_time - stats.deep_sleep_time) / 1000000;
+    sleep_time = (float) stats.sleep_time / 1000000;
+    deep_sleep_time = (float) stats.deep_sleep_time / 1000000;
+    printf("active: %f, sleep: %f, deep sleep: %f", active_time, sleep_time, deep_sleep_time);
+    node_curr = ACT_MODE_CUR * active_time + SLP_MODE_CUR * sleep_time + DSLP_MODE_CUR * deep_sleep_time;
+    node_pwr = node_curr * 3.3 / 0.8;
+    printf("energy: %f J\n", node_pwr);
     switch (pwr_status) {
         case ECO_MODE:
+            if (avg_cap_curr_val > 2.0 & soc > 90.0) {
+                pwr_status = HARVEST_MODE;
+                printf("changed to harvest mode");
+            }
+            while(node_curr > avg_cap_curr_val * cap_vol_val * 0.8 / 3.3) {
+                dyn_samp_rate = dyn_samp_rate + 1s;
+            }
+
+
             break;
         case HARVEST_MODE:
+            if (avg_cap_curr_val < 1.0 || soc < 80.0) {
+                pwr_status = ECO_MODE;
+                printf("changed to eco mode");
+            }
             break;
     }
 
@@ -95,7 +136,8 @@ static void read_voltages() {
 static void send_voltages() {
     soc = (cap_vol_val * cap_vol_val - 0.25) / 7.29 * 100.0;
     printf("Voltage: %.3fV, Current: %.3fmA, StateOfCharge: %.3f%%\n", cap_vol_val, solar_cur_val, soc);
-    printf("AverageCurrent: %.3f\n", cap_avg_curr->filter(solar_cur_val));
+    avg_cap_curr_val = cap_avg_curr->filter(solar_cur_val);
+    printf("AverageCurrent: %.3f\n", avg_cap_curr_val);
 }
 
 static void read_sensor_data() {
@@ -178,8 +220,10 @@ int configLora() {
 
 int main()
 {   
+    fs.mount(&sd);
     sensor->wakeUp();
-    //configLora();
+
+    configLora();
     solar_voltage.set_reference_voltage(3.3);
     cap_voltage.set_reference_voltage(3.3);
     ev_queue.call_every(1s, read_voltages);
@@ -234,13 +278,11 @@ static void lora_event_handler(lorawan_event_t event)
             // try again
             if (MBED_CONF_LORA_DUTY_CYCLE_ON) {
                 send_lora_message();
-                printf("sneed seeds and feeds");
             }
             break;
         case RX_DONE:
             printf("\r\n Received message from Network Server \r\n");
             //receive_message();
-            printf("sneed seeds and feeds");
             break;
         case RX_TIMEOUT:
         case RX_ERROR:
@@ -253,7 +295,6 @@ static void lora_event_handler(lorawan_event_t event)
             printf("\r\n Uplink required by NS \r\n");
             if (MBED_CONF_LORA_DUTY_CYCLE_ON) {
                 send_lora_message();
-                printf("sneed seeds and feeds");
             }
             break;
         default:
